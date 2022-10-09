dd0ea4d86f0c025b620f06fa62071771ad548a36
Patrick Cruz <patrickezequielscruz@gmail.com>
2022-10-09 20:17:42 +0200

    🏳️ finished assignment

--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
@@ -5,8 +5,18 @@ import { TurnUserAdminUseCase } from "./TurnUserAdminUseCase";
 class TurnUserAdminController {
   constructor(private turnUserAdminUseCase: TurnUserAdminUseCase) {}
 
-  handle(request: Request, response: Response): Response {
-    // Complete aqui
+  handle(request: Request, response: Response): any {
+    const { user_id } = request.params;
+
+    try {
+      const user = this.turnUserAdminUseCase.execute({
+        user_id: String(user_id),
+      });
+
+      return response.json(user);
+    } catch (err) {
+      return response.status(404).json({ error: err });
+    }
   }
 }
 
